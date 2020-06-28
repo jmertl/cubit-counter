279171d9a7f929367f8661e9213d38dda80dfe44
Jakub Mertl <mertljakub@gmail.com>
2020-06-28 15:04:59 +0200

    preparation for firestore

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Counter
 
 A project with simple counter which uses new state management named Cubit
+
+## This project uses firestore
+
+* ANDROID: Put your "google-services.json" to the android/app (https://firebase.google.com/docs/android/setup#add_the_sdk)
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:counter/counter_cubit.dart';
+import 'package:counter/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit/flutter_cubit.dart';
 
-import 'counter_page.dart';
+import 'app/counter_page.dart';
 
 void main() => runApp(CubitCounter());
 
